b9ca5d1ff51e47e3f91fd790dfd35284eaa539dc
ivanneto02 <ivan.a.neto@hotmail.com>
2023-02-02 00:11:05 -0800

    Modifying exit to include status parameter
    Both exit funcitons and exit() calls were modified
    
    Part 2 & Part 3

--- getparents.c
+++ getparents.c
@@ -5,5 +5,5 @@
 int main(int argc, char* argv[]) {
     // printf("Testing");
     getparents();
-    exit();
+    exit(0);
 }
--- sysproc.c
+++ sysproc.c
@@ -16,7 +16,7 @@ sys_fork(void)
 int
 sys_exit(void)
 {
-  exit();
+  exit(myproc()->exitStatus);
   return 0;  // not reached
 }
 
